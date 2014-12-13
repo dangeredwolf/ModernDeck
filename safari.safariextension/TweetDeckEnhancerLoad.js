fb32c75ca9ea3b425d7c7d6f841b8dddc93d5173
ryandolan123 <ryandolan123@icloud.com>
2014-12-13 15:02:13 -0500

    early safari build of tweetdeck enhancer

---
+++ safari.safariextension/TweetDeckEnhancerLoad.js
@@ -0,0 +1,123 @@
+// TweetDeckEnhancerLoad.js
+// Copyright (c) 2014 Ryan Dolan (ryandolan123)
+
+if (typeof safari !== "undefined") {
+  var isSafari = true;
+  chrome = [];
+  chrome.extension = [];
+  chrome.extension.getURL = function(url){return safari.extension.baseURI + url;} //wow i am lazy
+}
+
+if (typeof chrome !== "undefined") {
+  var isChrome = true;
+}
+
+var links = document.getElementsByTagName("link");
+
+for (i = 0; i < links.length; i++) { 
+    if (typeof links[i].href !== "undefined") {
+    	if (links[i].href.substring(0,52) === "https://ton.twimg.com/tweetdeck-web/web/css/app-dark") {
+        if (isChrome) {
+      		links[i].href = chrome.extension.getURL("resources/app-dark.css"); // Inject custom style sheet (Dark Theme)
+        } else {
+          if (isSafari) {
+            links[i].href = safari.extension.baseURI + "resources/app-dark.css";
+          }
+        }
+      }
+      if (links[i].href.substring(0,53) === "https://ton.twimg.com/tweetdeck-web/web/css/app-light") {
+        if (isChrome) {
+          links[i].href = chrome.extension.getURL("resources/app-light.css"); // Inject custom style sheet (Light Theme)
+        } else {
+          if (isSafari) {
+            links[i].href = safari.extension.baseURI + "resources/app-light.css";
+          }
+        }
+      }
+    }
+}
+
+/*var scrs = document.getElementsByTagName("script");
+
+for (i = 0; i < scrs.length; i++) { 
+    if (typeof scrs[i].src !== "undefined") {
+      if (scrs[i].src.substring(0,55) === "https://ton.twimg.com/tweetdeck-web/web/scripts/default") {
+        scrs[i].src = chrome.extension.getURL("resources/default-debug.js"); // testing
+      }
+    }
+}*/
+
+var AudioSources = document.getElementsByTagName("source");
+
+for (i = 0; i < AudioSources.length; i++) { 
+  AudioSources[i].remove();
+}
+
+var NotificationSound = document.getElementsByTagName("audio")[0];
+NotificationSound.src = "https://ryandolan123.com/assets/tweetdeck/img/alert_2.mp3";
+
+InjectScript = document.createElement("script");
+InjectScript.src = chrome.extension.getURL("resources/TDEinject.js");
+InjectScript.type = "text/javascript";
+document.head.appendChild(InjectScript);
+
+InjectFonts = document.createElement("style");
+InjectFonts.innerHTML = "@font-face{font-family:'RobotoDraft';font-style:normal;font-weight: 300;src:local('RobotoDraft Light'),local('RobotoDraft-Light'),url(" + chrome.extension.getURL("resources/fonts/Roboto300latinext.woff2") + ") format('woff2');unicode-range:U+0100-024F,U+1E00-1EFF,U+20A0-20AB,U+20AD-20CF,U+2C60-2C7F,U+A720-A7FF;}@font-face{font-family:'RobotoDraft';\
+  font-style: normal;\
+  font-weight: 300;\
+  src: local('RobotoDraft Light'), local('RobotoDraft-Light'), url(" + chrome.extension.getURL("resources/fonts/Roboto300latin.woff2") + ") format('woff2');\
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;\
+}\
+/* latin-ext */\
+@font-face {\
+  font-family: 'RobotoDraft';\
+  font-style: normal;\
+  font-weight: 400;\
+  src: local('RobotoDraft'), local('RobotoDraft-Regular'), url(" + chrome.extension.getURL("resources/fonts/Roboto400latinext.woff2") + ") format('woff2');\
+  unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;\
+}\
+/* latin */\
+@font-face {\
+  font-family: 'RobotoDraft';\
+  font-style: normal;\
+  font-weight: 400;\
+  src: local('RobotoDraft'), local('RobotoDraft-Regular'), url(" + chrome.extension.getURL("resources/fonts/Roboto400latin.woff2") + ") format('woff2');\
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;\
+}\
+/* latin-ext */\
+@font-face {\
+  font-family: 'RobotoDraft';\
+  font-style: normal;\
+  font-weight: 500;\
+  src: local('RobotoDraft Medium'), local('RobotoDraft-Medium'), url(" + chrome.extension.getURL("resources/fonts/Roboto500latinext.woff2") + ") format('woff2');\
+  unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;\
+}\
+/* latin */\
+@font-face {\
+  font-family: 'RobotoDraft';\
+  font-style: normal;\
+  font-weight: 500;\
+  src: local('RobotoDraft Medium'), local('RobotoDraft-Medium'), url(" + chrome.extension.getURL("resources/fonts/Roboto500latin.woff2") + ") format('woff2');\
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;\
+}\
+/* latin-ext */\
+@font-face {\
+  font-family: 'RobotoDraft';\
+  font-style: normal;\
+  font-weight: 700;\
+  src: local('RobotoDraft Bold'), local('RobotoDraft-Bold'), url(" + chrome.extension.getURL("resources/fonts/Roboto700latinext.woff2") + ") format('woff2');\
+  unicode-range: U+0100-024F, U+1E00-1EFF, U+20A0-20AB, U+20AD-20CF, U+2C60-2C7F, U+A720-A7FF;\
+}\
+/* latin */\
+@font-face {\
+  font-family: 'RobotoDraft';\
+  font-style: normal;\
+  font-weight: 700;\
+  src: local('RobotoDraft Bold'), local('RobotoDraft-Bold'), url(" + chrome.extension.getURL("resources/fonts/Roboto700latin.woff2") + "2) format('woff2');\
+  unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2212, U+2215, U+E0FF, U+EFFD, U+F000;\
+}";
+document.head.appendChild(InjectFonts);
+
+setTimeout(function() {
+	document.getElementsByTagName("html")[0].class = document.getElementsByTagName("html")[0].class + " ryandolan123TweetDeckEnhancerExtensionChrome"; // I thought += worked in javascript? guess not. Either isn't not or I am really bad at this
+}, 1000);
---
+++ safari.safariextension/resources/TDEinject.js
@@ -0,0 +1,6 @@
+// TDEinject.js
+// Copyright (c) 2014 Ryan Dolan (ryandolan123)
+
+console.log("TDEinject loaded");
+
+// TODO: do things here
