3b6f3a165cbe868b7510ace1225599f62722d84f
ryandolan123 <ryandolan123@icloud.com>
2014-12-24 15:34:43 -0500

    fixed a tab that i didn't like

--- UniversalExtension.safariextension/resources/TDEinject.js
+++ UniversalExtension.safariextension/resources/TDEinject.js
@@ -6,7 +6,7 @@ console.log("TDEinject loaded");
 function WaitForTDToConfigureSelf(){
 	if (typeof document.getElementsByClassName("app-signin-form")[0] !== "undefined") {
 		document.getElementsByTagName("html")[0].setAttribute("class",document.getElementsByTagName("html")[0].getAttribute("class") + " signin-sheet-now-present");
-    	WaitForLogin();
+    WaitForLogin();
 	} else {
 		if (typeof document.getElementsByClassName("app-content")[0] === "undefined") {
 			setTimeout(WaitForTDToConfigureSelf,60);
