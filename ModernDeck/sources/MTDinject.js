881a2de86dcb6ebf6c0458ac299da5e1dfccfc96
dangeredwolf <ryandolan123@icloud.com>
2016-04-07 20:37:34 -0400

    S T U F F

--- ModernDeck/sources/MTDinject.js
+++ ModernDeck/sources/MTDinject.js
@@ -188,7 +188,7 @@ function MTDInit(){
 		fontParseHelper({weight:"400",name:"Roboto400latinext"}) +
 		fontParseHelper({weight:"500",name:"Roboto500latin",range:"U+0000-00FF,U+0131,U+0152-0153,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2212,U+2215,U+E0FF,U+EFFD,U+F000"}) +
 		fontParseHelper({weight:"500",name:"Roboto500latinext"}) +
-		fontParseHelper({family:"Material Icons",weight:"400",name:"MaterialIcons",range:"U+0000-F000"}) +
+		fontParseHelper({family:"Material",weight:"400",name:"MaterialIcons",range:"U+0000-F000"}) +
 		fontParseHelper({family:"Font Awesome",weight:"400",name:"fontawesome",range:"U+0000-F000"})
 	));
 
@@ -425,7 +425,7 @@ function MTDSettings() {
 			$("#mtd-round-avatars-control").attr("checked",localStorage.mtd_round_avatars === "true" && true || false);
 			$("#mtd-outlines-control").attr("checked",localStorage.mtd_outlines === "true" && true || false);
 			$("#mtd-dark-media-control").attr("checked",localStorage.mtd_dark_media === "true" && true || false);
-			$("#mtd-theme-control").val(localStorage.mtd_theme || default);
+			$("#mtd-theme-control").val(localStorage.mtd_theme || "default");
 
 
 			PrefsListener();
