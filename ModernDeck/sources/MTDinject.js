f34937776117a50d62b67ea2edfa40495244202e
dangeredwolf <ryandolan123@icloud.com>
2016-09-15 19:16:44 -0400

    Fixed version number

--- ModernDeck/sources/MTDinject.js
+++ ModernDeck/sources/MTDinject.js
@@ -5,7 +5,7 @@
 
 "use strict";
 
-var SystemVersion = "6.1 Development Build 2016.09.15.2";
+var SystemVersion = "6.1 Development Build 2016.09.15.1";
 var MTDBaseURL = "https://raw.githubusercontent.com/dangeredwolf/ModernDeck/master/ModernDeck/"; // Defaults to streaming if nothing else is available (i.e. legacy firefox)
 
 var msgID,
