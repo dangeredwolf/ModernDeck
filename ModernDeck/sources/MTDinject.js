7e10a878f4b91a875085e6991f6084db28041ca9
dangeredwolf <dangeredwolf@gmail.com>
2019-03-26 23:06:03 -0400

    6.5.1A: hotfix for an untested feature

--- ModernDeck/sources/MTDinject.js
+++ ModernDeck/sources/MTDinject.js
@@ -5,7 +5,7 @@
 
 "use strict";
 
-var SystemVersion = "6.5.1";
+var SystemVersion = "6.5.1A";
 var MTDBaseURL = "https://rawgit.com/dangeredwolf/ModernDeck/stable/ModernDeck/"; // Defaults to streaming if using online client
 
 var msgID,
