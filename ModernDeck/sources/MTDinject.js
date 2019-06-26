6692a80e835aadd4de18e3cddcea91e4d6cb8599
dangeredwolf <d@ngeredwolf.me>
2019-06-26 17:11:19 -0400

    ModernDeck 7.3.4

--- ModernDeck/sources/MTDinject.js
+++ ModernDeck/sources/MTDinject.js
@@ -9,7 +9,7 @@
 
 'use strict';
 
-const SystemVersion = "7.3.3";
+const SystemVersion = "7.3.4";
 const appendTextVersion = true;
 
 let mtdBaseURL = "https://raw.githubusercontent.com/dangeredwolf/ModernDeck/master/ModernDeck/";
