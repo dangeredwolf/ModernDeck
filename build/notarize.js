168582cf9ee7ddca65992392db3a6228b45751cb
dangeredwolf <d@ngeredwolf.me>
2020-03-16 01:36:08 -0400

    Update notarize.js

--- build/notarize.js
+++ build/notarize.js
@@ -1,3 +1,11 @@
+import { notarize } from 'electron-notarize';
+
+
 module.exports = async function (params) {
-	
+	await notarize({
+		"com.dangeredwolf.ModernDeck",
+		"./dist/mac/ModernDeck.app",
+		process.env.MDAppleID,
+		process.env.MDAppleAppPassword
+	});
 };
