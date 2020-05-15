7b8abbb1bf0aed726cf4fab255161a1ae42389bd
dangered wolf <d@ngeredwolf.me>
2020-05-15 00:33:23 -0400

    Removed request blocking due to Chrome Web Store bullshit

--- common/extension/MTDBackground.js
+++ common/extension/MTDBackground.js
@@ -16,25 +16,21 @@ if (browser !== "undefined") {
 
 		for (i = 0; i < details.responseHeaders.length; i++) {
 			if (typeof details.responseHeaders[i].name !== "undefined" && details.responseHeaders[i].name === "content-security-policy") {
-				details.responseHeaders[i].value = "default-src 'self'; connect-src *; font-src https: data: blob: *; frame-src https:; frame-ancestors 'self' https:; img-src https: data: blob:; media-src *; object-src 'self' https: data: blob:; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://c6.patreon.com https://moderndeck.org https://sentry.io https://cdn.jsdelivr.net https://ajax.googleapis.com https://cdn.ravenjs.com/ https://*.twitter.com https://*.twimg.com https://api-ssl.bitly.com; style-src 'self' 'unsafe-inline' https:;";
+				details.responseHeaders[i].value = "default-src 'self'; connect-src * https://api.moderndeck.org; font-src https: data: blob: *; frame-src https:; frame-ancestors 'self' https:; img-src https: data: blob:; media-src *; object-src 'self' https: data: blob:; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://c6.patreon.com https://moderndeck.org https://sentry.io https://cdn.jsdelivr.net https://ajax.googleapis.com https://cdn.ravenjs.com/ https://*.twitter.com https://*.twimg.com https://api-ssl.bitly.com; style-src 'self' 'unsafe-inline' https:;";
 				return {responseHeaders:details.responseHeaders};
 			}
 		}
 
-	}, {urls:["https://tweetdeck.twitter.com/*","https://twitter.com/i/cards/*"]}, ["responseHeaders","blocking"]);
+	}, {urls:["https://tweetdeck.twitter.com/*","https://twitter.com/i/cards/*"]});
 
 	browser.webRequest.onBeforeRequest.addListener((details) => {
-
-		if (details.url.indexOf(".css") > -1 && (details.url.indexOf("bundle") > -1 && details.url.indexOf("dist") > -1)) {
-			return {cancel:true};
-		}
 
 		if (details.url.indexOf(".css") > -1 && (details.url.indexOf("tfw/css") > -1 && details.url.indexOf("tweetdeck_bundle") > -1)) {
 			return ({redirectURL:browser.runtime.getURL("sources/cssextensions/twittercard.css")});
 		}
 
 		return;
-	}, {urls:["https://ton.twimg.com/*"]}, ["blocking","requestBody"]);
+	}, {urls:["https://ton.twimg.com/*"]});
 
 	browser.runtime.onMessage.addListener((m) => {
 		console.log("Message received");
