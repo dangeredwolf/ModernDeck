970afc8514033cd2aeb3c7267b230111293e6ae0
dangered wolf <d@ngeredwolf.me>
2020-05-21 12:10:50 -0400

    Update MTDBackground.js

--- common/extension/MTDBackground.js
+++ common/extension/MTDBackground.js
@@ -7,19 +7,16 @@
 
 var browser = browser || chrome;
 
-if (browser !== "undefined") {
+browser.webRequest.onHeadersReceived.addListener((details) => {
+	if (details.type !== "main_frame" && details.type !== "sub_frame") {
+		return;
+	}
 
-	browser.webRequest.onHeadersReceived.addListener((details) => {
-		if (details.type !== "main_frame" && details.type !== "sub_frame") {
-			return;
+	for (i = 0; i < details.responseHeaders.length; i++) {
+		if (typeof details.responseHeaders[i].name !== "undefined" && details.responseHeaders[i].name === "content-security-policy") {
+			details.responseHeaders[i].value = "default-src 'self'; connect-src * https://api.moderndeck.org; font-src https: data: blob: *; frame-src https:; frame-ancestors 'self' https:; img-src https: data: blob:; media-src *; object-src 'self' https: data: blob:; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://c6.patreon.com https://moderndeck.org https://sentry.io https://cdn.jsdelivr.net https://ajax.googleapis.com https://cdn.ravenjs.com/ https://*.twitter.com https://.twimg.com https://api-ssl.bitly.com; style-src 'self' 'unsafe-inline' https:;";
+			return {responseHeaders:details.responseHeaders};
 		}
+	}
 
-		for (i = 0; i < details.responseHeaders.length; i++) {
-			if (typeof details.responseHeaders[i].name !== "undefined" && details.responseHeaders[i].name === "content-security-policy") {
-				details.responseHeaders[i].value = "default-src 'self'; connect-src * https://api.moderndeck.org; font-src https: data: blob: *; frame-src https:; frame-ancestors 'self' https:; img-src https: data: blob:; media-src *; object-src 'self' https: data: blob:; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://c6.patreon.com https://moderndeck.org https://sentry.io https://cdn.jsdelivr.net https://ajax.googleapis.com https://cdn.ravenjs.com/ https://*.twitter.com https://*.twimg.com https://api-ssl.bitly.com; style-src 'self' 'unsafe-inline' https:;";
-				return {responseHeaders:details.responseHeaders};
-			}
-		}
-
-	}, {urls:["https://tweetdeck.twitter.com/*","https://twitter.com/i/cards/*"]});
-}
+}, {urls:["https://tweetdeck.twitter.com/*","https://twitter.com/i/cards/*"]});
