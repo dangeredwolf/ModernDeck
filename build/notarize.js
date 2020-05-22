a5e8a5b8e4e3bb77cec5daae4e83166f339f2723
dangered wolf <d@ngeredwolf.me>
2020-05-21 20:55:33 -0400

    Update notarize.js

--- build/notarize.js
+++ build/notarize.js
@@ -1,13 +1,14 @@
 const {notarize} = require('electron-notarize');
 
-console.log("Notarizing app... this will take a while...");
-console.log("Seriously. it'll be a while. Go use the bathroom, maybe grab a snack or something. You might need it.");
-
 module.exports = async function (params) {
 	const { electronPlatformName } = context;
 	if (electronPlatformName !== "darwin") {
 		return;
 	}
+
+	console.log("Notarizing macOS app... this will take a while...");
+	console.log("Seriously. it'll be a while. Go use the bathroom, maybe grab a snack or something. You might need it.");
+	
 	await notarize({
 		appBundleId: "com.dangeredwolf.ModernDeck",
 		appPath: "./dist/mac/ModernDeck.app",
