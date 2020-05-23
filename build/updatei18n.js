5554635d4bc2ce07ba3d64c9816af41f27acfc11
dangered wolf <d@ngeredwolf.me>
2020-05-23 12:13:47 -0400

    fixed a bug and added an error check

--- build/updatei18n.js
+++ build/updatei18n.js
@@ -21,13 +21,17 @@ let newObj = {};
 
 let langMap = ["src","af","bg","ca","zh_CN","zh_TW","hr","cs","da","nl","en","en_CA","en_GB","en_US","et","fi","fr","fr_CA","de","el","hi","hu","it","ja","ko","mi","no","pl","pt","pt_BR","ro","ru","sr","es","es_AR","es_419","es_US","sv","tr","uk","vi","eo"];
 
-buildFile.split("\n").forEach((a, i) => {
+buildFile.split("\n").forEach((a, j) => {
 	let arr = a.replace(/\"\,\"/g,"\"=,=\"").replace(/\=\"\"/g,"=\"").replace(/\"\" /g,"\" ").replace(/\"\"\>/g,"\">").replace(/ \"\"/g," \"").replace(/\"\" /g,"\" ").split("=,=");
 	if (arr[0]) {
 		let key = arr[0].substr(1, arr[0].length-2);
 		newObj[key] = {};
 		for (let i = 1; i < langMap.length; i++) {
-			newObj[key][langMap[i]] = arr[i].substr(1, arr[i].length-2).replace(/\"\"/g,"\"");
+			if (arr[i]) {
+				newObj[key][langMap[i]] = arr[i].substr(1, arr[i].length-2).replace(/\"\"/g,"\"");
+			} else {
+				console.log("Language array missing items: Line " + j + " Column " + i)
+			}
 		}
 	}
 });
