704475643012c33cedef1490ce74244336ab9d3a
dangered wolf <d@ngeredwolf.me>
2020-07-18 16:45:41 -0400

    Update dependencies (closes #165, #167)

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 
 export default {
