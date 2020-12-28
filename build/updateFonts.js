bcb74a9851fc083dcc817061accfe499b9a954bf
dangered wolf <d@ngeredwolf.me>
2020-12-28 01:50:09 -0500

    Update updateFonts.js

--- build/updateFonts.js
+++ build/updateFonts.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 const fs = require("fs");
 
 const axiosConfig = {
+	responseType: "arraybuffer",
 	headers: {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"}
 }
 
@@ -17,26 +18,56 @@ const charsetTranslation = {
 
 console.log("Updating Fonts from Google Fonts...");
 
-function processFonts(data) {
-	let entries = data.split("/* ");
+async function processFonts(data, fontName) {
+	let entries = (data + "").split("/* ");
 
 	let output = {};
 
-	entries.forEach(entry => {
+	entries.forEach(async entry => {
 		let name = entry.match(/^[\w\-]+/g);
 		let weight = entry.match(/(?<=font-weight: )\d+/g);
 		let url = entry.match(/(?<=src: url\()https:\/\/[\w\.\/\-]+(?=\))/g);
 
-		console.log(name ? name[0] : undefined, weight ? weight[0] : undefined, url ? url[0] : undefined);
+		if (!name) {
+			console.error("Missing charset name for the following entry: " + entry);
+			return;
+		}
+		if (!weight) {
+			console.error("Missing weight for the following entry: " + entry);
+			return;
+		}
+		if (!url) {
+			console.error("Missing url for the following entry: " + entry);
+			return;
+		}
+
+		let saveTo = `${__dirname}/../common/resources/fonts/${fontName}/${weight[0]}\-${charsetTranslation[name[0]]}.woff2`;
+
+		if (process.platform === "win32") {
+			saveTo = saveTo.replace(/\//g, "\\");
+		}
+
+		console.log(`Saving ${url[0]} as ${saveTo}`);
+
+		let fetched = await axios.get(url[0], axiosConfig);
+		let fetchedData = fetched.data;
+
+		if (typeof fetchedData !== "undefined") {
+			fs.writeFileSync(saveTo, fetched.data)
+		} else {
+			console.error("fetchedData is undefined....");
+		}
+
+
 	})
 
 
 }
 
 async function updateFonts() {
-	let Roboto = await axios.get("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap", axiosConfig);
+	let Roboto = await axios.get("https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,500;0,700;0,900&display=swap", axiosConfig);
 
-	console.log(processFonts(Roboto.data));
+	console.log(processFonts(Roboto.data, "Roboto"));
 }
 
 updateFonts();
