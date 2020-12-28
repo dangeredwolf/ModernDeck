6931c8e6e8ba119364e6a039aeae02fbed19a51e
dangered wolf <d@ngeredwolf.me>
2020-12-28 01:50:57 -0500

    Added CJK black font

--- src/FontHandler.js
+++ src/FontHandler.js
@@ -129,6 +129,7 @@ export function injectFonts() {
 		fontParseHelper({family:"Lato",weight:"700",name:"Lato/700-latin",range:"U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD"}) +
 
 		/* Noto Sans / Noto Sans CJK Family */
+		fontParseHelper({family:"Noto Sans CJK",weight:"700",name:"NotoSans/NotoSansCJKjp-Black",format:"opentype",extension:"otf"}) +
 		fontParseHelper({family:"Noto Sans CJK",weight:"500",name:"NotoSans/NotoSansCJKjp-Medium",format:"opentype",extension:"otf"}) +
 		fontParseHelper({family:"Noto Sans CJK",name:"NotoSans/NotoSansCJKjp-Regular",format:"opentype",extension:"otf"}) +
 		fontParseHelper({family:"Noto Sans",weight:"500",name:"NotoSans/NotoSansHI-Medium",range:"U+0900-097F"}) +
