7983d3f07194f81c22ff5207cc9b825ecd04a9bd
dangered wolf <d@ngeredwolf.me>
2022-04-14 04:00:36 -0400

    Moved AdvancedMuteEngine back to TS version

--- src/AME/AdvancedMuteEngine.ts
+++ src/AME/AdvancedMuteEngine.ts
@@ -1,6 +1,7 @@
 /*
  * Advanced Mute Engine for TweetDeck
  * Copyright (c) 2017 pixeldesu
+ * Converted to TypeScript by eramdam
  * 
  * Based off of the modifications made by Damien Erambert for Better TweetDeck
  *
@@ -27,156 +28,200 @@
  *
  */
 
-import { getPref, setPref } from "./StoragePreferences";
+import { ChirpBaseTypeEnum, TweetDeckChirp, TweetDeckFilter, TweetDeckFilterTypes, TweetDeckObject } from '../Types/TweetDeck';
 
 /*
   ModernDeck uses the BTD namespace for AME to enable interoperability between them,
   and so mutes will work across them whenever ModernDeck fully supports AME in the future.
 */
 
-const AMEFilters = {
-  NFT_AVATAR: 'BTD_nft_avatar',
-  // IS_RETWEET_FROM: 'BTD_is_retweet_from',
-  // MUTE_USER_KEYWORD: 'BTD_mute_user_keyword',
-  // REGEX_DISPLAYNAME: 'BTD_mute_displayname',
-  // REGEX: 'BTD_regex',
-  // USER_REGEX: 'BTD_user_regex',
-  // MUTE_QUOTES: 'BTD_mute_quotes',
-  // USER_BIOGRAPHIES: 'BTD_user_biographies',
-  // DEFAULT_AVATARS: 'BTD_default_avatars',
-  // FOLLOWER_COUNT_LESS_THAN: 'BTD_follower_count_less_than',
-  // FOLLOWER_COUNT_GREATER_THAN: 'BTD_follower_count_greater_than',
-  // SPECIFIC_TWEET: 'BTD_specific_tweet',
+export enum AMEFilters {
+	NFT_AVATAR = 'BTD_nft_avatar',
+	IS_RETWEET_FROM = 'BTD_is_retweet_from',
+	MUTE_USER_KEYWORD = 'BTD_mute_user_keyword',
+	REGEX_DISPLAYNAME = 'BTD_mute_displayname',
+	REGEX = 'BTD_regex',
+	USER_REGEX = 'BTD_user_regex',
+	MUTE_QUOTES = 'BTD_mute_quotes',
+	USER_BIOGRAPHIES = 'BTD_user_biographies',
+	DEFAULT_AVATARS = 'BTD_default_avatars',
+	FOLLOWER_COUNT_LESS_THAN = 'BTD_follower_count_less_than',
+	FOLLOWER_COUNT_GREATER_THAN = 'BTD_follower_count_greater_than',
+	SPECIFIC_TWEET = 'BTD_specific_tweet',
+	HASHTAGS_NUMBER = 'BTD_hashtags_number',
+}
+
+import * as t from 'io-ts';
+import { getPref } from '../StoragePreferences';
+
+declare let TD: TweetDeckObject;
+
+interface AMEFilter {
+  name: string;
+  descriptor: string;
+  placeholder: string;
+  display?: AMEDisplayOptions;
+  options?: AMEFilterOptions;
+  function(filter: TweetDeckObject['vo']['Filter'], chirp: TweetDeckChirp): boolean;
+}
+
+interface AMEDisplayOptions {
+  global?: boolean;
+  actions?: boolean;
+  options?: boolean;
+}
+
+interface AMEFilterOptions {
+  templateString?: string;
+  nameInDropdown?: string;
+}
+
+const RMuteCatch = t.type({
+	filterType: t.union([makeEnumRuntimeType(AMEFilters), makeEnumRuntimeType(TweetDeckFilterTypes)]),
+	value: t.string,
+	user: t.type({
+		avatar: t.string,
+		id: t.string,
+		screenName: t.string,
+		name: t.string,
+	}),
+});
+
+export interface MuteCatch extends t.TypeOf<typeof RMuteCatch> {
+	filterType: any;
 }
 
 const nonUserSpecificsTypes = [
-  "source", // TD.vo.Filter.SOURCE
-  "phrase", // TD.vo.Filter.SOURCE
-  AMEFilters.REGEX,
+	TweetDeckFilterTypes.SOURCE,
+	TweetDeckFilterTypes.PHRASE,
+	AMEFilters.REGEX,
 ];
 
 const userSpecificTypes = [
-  // AMEFilters.DEFAULT_AVATARS,
-  // AMEFilters.FOLLOWER_COUNT_GREATER_THAN,
-  // AMEFilters.FOLLOWER_COUNT_LESS_THAN,
-  // AMEFilters.MUTE_USER_KEYWORD,
-  AMEFilters.NFT_AVATAR,
-  // AMEFilters.REGEX_DISPLAYNAME,
-  // AMEFilters.USER_BIOGRAPHIES,
-  // AMEFilters.USER_REGEX,
+	AMEFilters.DEFAULT_AVATARS,
+	AMEFilters.FOLLOWER_COUNT_GREATER_THAN,
+	AMEFilters.FOLLOWER_COUNT_LESS_THAN,
+	AMEFilters.MUTE_USER_KEYWORD,
+	AMEFilters.NFT_AVATAR,
+	AMEFilters.REGEX_DISPLAYNAME,
+	AMEFilters.USER_BIOGRAPHIES,
+	AMEFilters.USER_REGEX,
 ];
 
-const muteTypeAllowlist = [...nonUserSpecificsTypes, ...userSpecificTypes];
+const muteTypeAllowlist = [...nonUserSpecificsTypes, ...userSpecificTypes] as const;
+type AllowedMuteTypes = typeof muteTypeAllowlist[number];
 
 function getInitialMuteCatches() {
-  const fromLocalStorage = (getPref("mtd_ame_mute_catches", []) || []).filter((c) => RMuteCatch.is(c));
-  return new Map(fromLocalStorage);
+	const fromLocalStorage = getPref("mtd_mute_catches");
+	return new Map<string, MuteCatch>(fromLocalStorage);
 }
 
 const muteCatches = getInitialMuteCatches();
 
-const clearMuteCatches = () => {
-  muteCatches.clear();
+export const clearMuteCatches = () => {
+	muteCatches.clear();
 };
 
-export function encodeCatchKey(muteCatch) {
-  return [muteCatch.filterType, muteCatch.user.id, encodeURIComponent(muteCatch.value)].join('$_$');
+export function removeCatchesByFilter(filter: { type: string; value: string }) {
+	Array.from(muteCatches.entries()).forEach(([key, value]) => {
+		if (value.filterType === filter.type && value.value === filter.value) {
+			muteCatches.delete(key);
+		}
+	});
 }
 
-export function encodeMuteReasonKey(muteReason) {
-  return [muteReason.filterType, encodeURIComponent(muteReason.value)].join('$_$');
-}
-export function decodeMuteReasonKey(muteReasonKey) {
-  const [filterType, rawValue] = muteReasonKey.split('$_$');
-
-  return {
-    filterType: filterType,
-    value: decodeURIComponent(rawValue),
-  };
+function getMeaningfulUser(target: TweetDeckChirp) {
+	return (
+		target.retweetedStatus?.user ||
+		target.sourceUser ||
+		target.user ||
+		target.following ||
+		target.owner
+	);
 }
 
-function getMeaningfulUser(target) {
-  return (
-    target.retweetedStatus?.user ||
-    target.sourceUser ||
-    target.user ||
-    target.following ||
-    target.owner || []
+function serializeMuteCatch(target: TweetDeckChirp, filter: TweetDeckFilter): MuteCatch {
+	const meaningfulUser = getMeaningfulUser(target);
+	if (!meaningfulUser) {
+		console.debug(filter, target);
+	}
+
+	const simplifiedUser = {
+		avatar: meaningfulUser.profileImageURL,
+		id: meaningfulUser.id,
+		screenName: meaningfulUser.screenName,
+		name: meaningfulUser.name,
+	};
+
+	return {
+		filterType: filter.type as AllowedMuteTypes,
+		value: filter.value,
+		user: simplifiedUser,
+	};
+}
+
+export function encodeCatchKey(muteCatch: MuteCatch) {
+	return [muteCatch.filterType, muteCatch.user.id, encodeURIComponent(muteCatch.value)].join('$_$');
+}
+export function maybeLogMuteCatch(
+	target: TweetDeckChirp,
+	filter: TweetDeckFilter,
+	shouldDisplay: boolean
+) {
+	return new Promise<void>((resolve) => {
+		// If the filter isn't part of our allowlist, nothing to do.
+		if (!muteTypeAllowlist.includes(filter.type as any)) {
+			return resolve();
+		}
+		// Serialize our catch for easy storage
+		const serialized = serializeMuteCatch(target, filter);
+
+		// Make a unique key based on target+filter+value
+		const catchKey = encodeCatchKey(serialized);
+
+		if (muteCatches.has(catchKey)) {
+			// If the target was previously matched and isn't anymore, then we can remove them.
+			if (shouldDisplay && !nonUserSpecificsTypes.includes(filter.type)) {
+				muteCatches.delete(catchKey);
+			}
+			// Otherwise, we can stop here
+			return resolve();
+		}
+
+		if (shouldDisplay) {
+			return resolve();
+		}
+
+		// If we have a user-specific filter type, make sure we're logging the right user.
+		if (
+			userSpecificTypes.includes(filter.type as unknown as AMEFilters) &&
+			getMeaningfulUser(target).screenName !== target.user.screenName
+		) {
+			return resolve();
+		}
+
+		muteCatches.set(catchKey, serialized);
+		return resolve();
+	});
+}
+
+export function makeEnumRuntimeType<T extends Object>(srcEnum: object) {
+  const enumValues = new Set(Object.values(srcEnum));
+  return new t.Type<T, string>(
+    'Enum',
+    (value: any): value is T => Boolean(value && enumValues.has(value)),
+    (value, context) => {
+      if (!value || !enumValues.has(value)) return t.failure(value, context);
+
+      return t.success(value as any as T);
+    },
+    (value) => value.toString()
   );
 }
 
-function serializeMuteCatch(target, filter)  {
-  const meaningfulUser = getMeaningfulUser(target);
-  // console.log(meaningfulUser);
-  if (!meaningfulUser) {
-    // console.debug(filter, target);
-  }
+type AMEFiltersMap = {[k in AMEFilters]: AMEFilter};
 
-  const simplifiedUser = {
-    avatar: meaningfulUser.profileImageURL,
-    id: meaningfulUser.id,
-    screenName: meaningfulUser.screenName,
-    name: meaningfulUser.name,
-  };
-
-  return {
-    filterType: filter.type,
-    value: filter.value,
-    user: simplifiedUser,
-  };
-}
-
-function maybeLogMuteCatch(
-  target,
-  filter,
-  shouldDisplay
-) {
-  return new Promise((resolve) => {
-    // If the filter isn't part of our allowlist, nothing to do.
-    if (!muteTypeAllowlist.includes(filter.type)) {
-      return resolve();
-    }
-    // Serialize our catch for easy storage
-    const serialized = serializeMuteCatch(target, filter);
-
-    // Make a unique key based on target+filter+value
-    const catchKey = encodeCatchKey(serialized);
-
-    if (muteCatches.has(catchKey)) {
-      // If the target was previously matched and isn't anymore, then we can remove them.
-      if (shouldDisplay && !nonUserSpecificsTypes.includes(filter.type)) {
-        muteCatches.delete(catchKey);
-      }
-      // Otherwise, we can stop here
-      return resolve();
-    }
-
-    if (shouldDisplay) {
-      return resolve();
-    }
-
-    // If we have a user-specific filter type, make sure we're logging the right user.
-    if (
-      userSpecificTypes.includes(filter.type) &&
-      getMeaningfulUser(target).screenName !== target.user.screenName
-    ) {
-      return resolve();
-    }
-
-    muteCatches.set(catchKey, serialized);
-    return resolve();
-  });
-}
-
-
-
-export function removeCatchesByFilter(filter) {
-  Array.from(muteCatches.entries()).forEach(([key, value]) => {
-    if (value.filterType === filter.type && value.value === filter.value) {
-      muteCatches.delete(key);
-    }
-  });
-}
+export const RAMEFilters = makeEnumRuntimeType<AMEFilters>(AMEFilters);
 
 export const setupAME = () => {
   // Save references of original functions
@@ -187,8 +232,8 @@ export const setupAME = () => {
   TD.controller.filterManager._removeFilter = TD.controller.filterManager.removeFilter;
 
   // Custom filters
-  const AmeFilters = {
-    NFT_AVATAR: {
+  const AmeFilters: AMEFiltersMap = {
+    [AMEFilters.NFT_AVATAR]: {
       display: {
         global: false,
         options: false,
@@ -205,7 +250,7 @@ export const setupAME = () => {
         return e.user.hasNftAvatar === false;
       },
     },
-    SPECIFIC_TWEET: {
+    [AMEFilters.SPECIFIC_TWEET]: {
       name: 'Specific tweet',
       descriptor: 'specific tweet',
       placeholder: 'ID of tweet',
@@ -221,7 +266,7 @@ export const setupAME = () => {
         return true;
       },
     },
-    IS_RETWEET_FROM: {
+    [AMEFilters.IS_RETWEET_FROM]: {
       display: {
         actions: true,
       },
@@ -232,7 +277,7 @@ export const setupAME = () => {
         return !(e.isRetweetedStatus() && t.value === e.user.screenName.toLowerCase());
       },
     },
-    MUTE_USER_KEYWORD: {
+    [AMEFilters.MUTE_USER_KEYWORD]: {
       display: {
         global: true,
       },
@@ -250,7 +295,26 @@ export const setupAME = () => {
         );
       },
     },
-    REGEX_DISPLAYNAME: {
+    [AMEFilters.HASHTAGS_NUMBER]: {
+      display: {
+        global: true,
+      },
+      name: 'Tweet contains more than X hashtags',
+      descriptor: 'tweets with more than X hashtags',
+      placeholder: 'Enter a number',
+      function(t, e) {
+        if (!e.entities) {
+          return true;
+        }
+
+        if (!Number.isSafeInteger(Number(t.value))) {
+          return true;
+        }
+
+        return e.entities.hashtags.length <= Number(t.value);
+      },
+    },
+    [AMEFilters.REGEX_DISPLAYNAME]: {
       display: {
         global: true,
       },
@@ -264,7 +328,7 @@ export const setupAME = () => {
         return !e.user.name.match(regex);
       },
     },
-    REGEX: {
+    [AMEFilters.REGEX]: {
       display: {
         global: true,
       },
@@ -277,7 +341,7 @@ export const setupAME = () => {
         return !e.getFilterableText().match(regex);
       },
     },
-    USER_REGEX: {
+    [AMEFilters.USER_REGEX]: {
       display: {
         global: true,
       },
@@ -291,7 +355,7 @@ export const setupAME = () => {
         return !e.user.screenName.match(regex);
       },
     },
-    MUTE_QUOTES: {
+    [AMEFilters.MUTE_QUOTES]: {
       display: {
         actions: true,
       },
@@ -304,7 +368,7 @@ export const setupAME = () => {
         return !(e.isQuoteStatus && t.value === e.user.screenName.toLowerCase());
       },
     },
-    USER_BIOGRAPHIES: {
+    [AMEFilters.USER_BIOGRAPHIES]: {
       display: {
         global: true,
       },
@@ -317,7 +381,7 @@ export const setupAME = () => {
         return !e.user.description.toLowerCase().includes(t.value);
       },
     },
-    DEFAULT_AVATARS: {
+    [AMEFilters.DEFAULT_AVATARS]: {
       display: {
         global: true,
       },
@@ -330,7 +394,7 @@ export const setupAME = () => {
         return !e.user.profileImageURL.includes('default');
       },
     },
-    FOLLOWER_COUNT_LESS_THAN: {
+    [AMEFilters.FOLLOWER_COUNT_LESS_THAN]: {
       display: {
         global: true,
       },
@@ -343,7 +407,7 @@ export const setupAME = () => {
         return !(e.user.followersCount < parseInt(t.value, 10));
       },
     },
-    FOLLOWER_COUNT_GREATER_THAN: {
+    [AMEFilters.FOLLOWER_COUNT_GREATER_THAN]: {
       display: {
         global: true,
       },
@@ -358,23 +422,48 @@ export const setupAME = () => {
     },
   };
 
+  // Custom _getFilterTarget implementation to take RTs into account.
+  function getAMEFilterTarget(
+    e: TweetDeckChirp,
+    filterTarget: 'parent' | 'child',
+    filterType: AMEFilters
+  ) {
+    if (e.targetTweet && filterTarget !== 'parent') {
+      return e.targetTweet;
+    }
+
+    if (userSpecificTypes.includes(filterType) && e.retweetedStatus) {
+      return e.retweetedStatus;
+    }
+
+    return e;
+  }
+
   // Custom pass function to apply our filters
   TD.vo.Filter.prototype.pass = function pass(e) {
-    // console.log(this.type);
-    if (Object.values(AMEFilters).indexOf(this.type) !== -1) {
-      // console.log("Passing filter: " + this.type);
-      const t = this;
-      e = this._getFilterTarget(e);
+    if (RAMEFilters.is(this.type)) {
+      try {
+        const t = this;
+        e = getAMEFilterTarget(e, this.filterTarget, this.type);
 
-      const shouldDisplay = Object.values(AmeFilters)[Object.values(AMEFilters).indexOf(this.type)].function(t, e);
-      maybeLogMuteCatch(e, this, shouldDisplay);
+        // @ts-ignore
+        const shouldDisplay = AmeFilters[this.type].function(t, e);
+        if (e.chirpType === ChirpBaseTypeEnum.TWEET || e.chirpType === ChirpBaseTypeEnum.UNKNOWN) {
+          maybeLogMuteCatch(e, this, shouldDisplay);
+        }
 
-      return shouldDisplay;
+        return shouldDisplay;
+      } catch (e) {
+        console.error(e);
+        return true;
+      }
     }
 
     const shouldDisplay = this._pass(e);
 
-    maybeLogMuteCatch(e, this, shouldDisplay);
+    if (e.chirpType === ChirpBaseTypeEnum.TWEET || e.chirpType === ChirpBaseTypeEnum.UNKNOWN) {
+      maybeLogMuteCatch(e, this, shouldDisplay);
+    }
 
     return shouldDisplay;
   };
@@ -389,56 +478,12 @@ export const setupAME = () => {
 
   // Custom display type function to show proper description in filter list
   TD.vo.Filter.prototype.getDisplayType = function getDisplayType() {
-    if (Object.values(AMEFilters).indexOf(this.type)) {
-      return Object.values(AmeFilters)[Object.values(AmeFilters).indexOf(this.type)].descriptor;
+    if (RAMEFilters.is(this.type)) {
+      // @ts-ignore
+      return AmeFilters[this.type].descriptor;
     }
     return this._getDisplayType();
   };
-
-  // Helper function to build <option>s for the custom filters
-  const filterDropdown = function filterDropdown() {
-    const filters = Object.keys(AmeFilters);
-    let filterString = '';
-
-    filters.forEach((filter) => {
-      if (!AMEFilters[filter]) {
-        return;
-      }
-      const fil = AmeFilters[filter];
-      if (fil.display && fil.display.global) {
-        filterString += `<option value="${filter}">{{_i}}${fil.name}{{/i}}</option>`;
-      }
-    });
-
-    return filterString;
-  };
-
-  // Helper function to build <li>s for the actions dropdown
-  // const userDropdown = function userDropdown() {
-  //   const filters = Object.keys(AmeFilters);
-  //   let filterString = '';
-
-  //   filters.forEach((filter) => {
-  //     if (!AMEFilters[filter]) {
-  //       return;
-  //     }
-  //     const fil = AmeFilters[filter];
-  //     if (fil.display && fil.display.actions) {
-  //       const templateString =
-  //         fil.options && fil.options.templateString ? fil.options.templateString : '{{screenName}}';
-  //       const name =
-  //         fil.options && fil.options.nameInDropdown
-  //           ? fil.options.nameInDropdown
-  //           : `Mute ${fil.name}`;
-
-  //       filterString += `<li class="is-selectable">
-  //           <a href="#" data-mtd-filter="${filter}" data-mtd-value="${templateString}">{{_i}}${name}{{/i}}</a>
-  //         </li>`;
-  //     }
-  //   });
-
-  //   return filterString;
-  // };
 
   $(document).on('change', '.js-filter-types', (e) => {
     e.preventDefault();
@@ -446,7 +491,7 @@ export const setupAME = () => {
     const options = e.target.options;
     const filter = e.target.options[options.selectedIndex].value;
 
-    if (AMEFilters[filter]) {
+    if (RAMEFilters.is(filter)) {
       $('.js-filter-input').attr('placeholder', AmeFilters[filter].placeholder);
     }
   });
@@ -459,28 +504,4 @@ export const setupAME = () => {
     TD.controller.filterManager.addFilter(filter, value);
   });
 
-  // Add our custom filters to the filter dropdown
-  // TD.mustaches['settings/global_setting_filter.mustache'] = TD.mustaches[
-  //   'settings/global_setting_filter.mustache'
-  // ].replace('</select>', `${filterDropdown()}</select>`);
-
-  // Add our custom filters to the actions dropdown
-  // TD.mustaches['menus/actions.mustache'] = TD.mustaches['menus/actions.mustache'].replace(
-  //   '{{/isMuted}} ',
-  //   `{{/isMuted}} {{#user}} {{^isMe}} ${userDropdown()} {{/isMe}} {{/user}}`
-  //);
-
-  const filterKey = 'moderndeck_specific_tweet';
-  const filter = (AmeFilters || [])[filterKey];
-
-  if (filter?.options) {
-    // TD.mustaches['menus/actions.mustache'] = TD.mustaches['menus/actions.mustache'].replace(
-    //   '{{/isOwnChirp}}',
-    //   `{{/isOwnChirp}}
-    //         <li class="is-selectable">
-    //           <a href="#" action="_" data-mtd-filter="${filterKey}" data-mtd-value="${filter.options.templateString}">${filter.options.nameInDropdown}</a>
-    //         </li>
-    //       `
-    // );
-  }
-};
+}
