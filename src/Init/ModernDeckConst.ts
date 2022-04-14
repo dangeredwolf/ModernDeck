5f4b63ab313777156ac39dd8b8c6367b7e4db625
dangered wolf <d@ngeredwolf.me>
2022-04-14 04:06:21 -0400

    Fixed incorrect header in ModernDeckConst

--- src/Init/ModernDeckConst.ts
+++ src/Init/ModernDeckConst.ts
@@ -1,13 +1,14 @@
 /*
-	Init/ModernDeckConst.js
+	Init/ModernDeckConst.ts
 
 	Copyright (c) 2014-2022 dangered wolf, et al
 	Released under the MIT License
 */
 
 import { version } from "../../package.json";
-import buildId from "./../buildId";
+import buildId from "../buildId";
 import { getPlatformName, getProductName, getSystemName } from "./VersionController";
+
 export let ModernDeck = {
 	version: 10.0,
     versionString: version,
