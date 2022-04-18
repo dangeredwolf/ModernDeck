814982874f1533d577b1800e417570388c980fc5
dangered wolf <d@ngeredwolf.me>
2022-04-18 13:13:26 -0400

    Replaced buildId.ts with BuildProps.json

--- build/incrementBuild.js
+++ build/incrementBuild.js
@@ -1,16 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 
-let thePath = path.format({
-	dir: __dirname + path.sep + ".." + path.sep + "src",
-	base: "buildId.ts"
-});
-
 let ver = require("../src/BuildProps.json").buildId;
 ver++;
-fs.writeFileSync(thePath,"export default " + ver++ + ";")
 
-let thePath2 = path.format({
+let thePath = path.format({
 	dir: __dirname + path.sep + ".." + path.sep + "src",
 	base: "BuildProps.json"
 });
@@ -21,4 +15,4 @@ const buildProps = {
 	buildVersion: require("../package.json").version
 }
 
-fs.writeFileSync(thePath2, JSON.stringify(buildProps))
+fs.writeFileSync(thePath, JSON.stringify(buildProps))
