0a990d389d79faa6d92d3ae900e311f5a2558faa
dangered wolf <d@ngeredwolf.me>
2022-04-21 22:43:05 -0400

    New welcome screen and misc

--- src/UIWelcome.ts
+++ src/UIWelcome.ts
@@ -7,12 +7,15 @@
 
 /* Main thread for welcome screen */
 
-import { make } from "./Utils";
+import { isApp, make } from "./Utils";
 import { UIModal } from "./UIModal";
 import { I18n } from "./I18n";
-import { setPref } from "./StoragePreferences";
+import { getPref, setPref } from "./StoragePreferences";
+import { TwitterUserInternal } from "./Types/TweetDeck";
+import { SettingsDropdown } from "./Settings/UI/Components/Dropdown";
+import themeSettings from "./Settings/Data/Themes";
 
-export const debugWelcome = false;
+export const debugWelcome = true;
 
 export function welcomeScreen() {
 	return new UIWelcome();
@@ -22,6 +25,36 @@ export class UIWelcome extends UIModal {
 	container: JQuery<HTMLElement>;
 	leftPane: JQuery<HTMLElement>;
 	rightPane: JQuery<HTMLElement>;
+	themeBox: JQuery<HTMLElement>;
+
+	developerInfoHTML(profile: TwitterUserInternal): string {
+		return `<header class="tweet-header js-tweet-header flex flex-row flex-align--baseline">
+			<a class="account-link link-complex block flex-auto" href="${profile.profileURL}" rel="user" target="_blank">
+				<div class="obj-left item-img tweet-img position-rel">
+					<img class="tweet-avatar avatar pin-top-full-width" src="${profile.profileImageURL}" alt="${profile.screenName}">
+				</div>
+				<div class="mtd-welcome-profile-text">
+					<b class="fullname link-complex-target">${profile.emojifiedName}</b>
+					<span class="username txt-mute">@${profile.screenName}</span>
+				</div>
+			</a>
+		</header>`
+	}
+
+	renderDeveloperInfo() {
+		// @ts-ignore types/jquery don't have addCallback under Deferred for some reason
+		TD.cache.twitterUsers.getById("2927859037").addCallback((profile: TwitterUserInternal) => { // @ModernDeck
+			console.log("Got details for ModernDeck", profile);
+			$("#moderndeck-profile").html(this.developerInfoHTML(profile));
+		});
+		// @ts-ignore types/jquery don't have addCallback under Deferred for some reason
+		TD.cache.twitterUsers.getById("3784131322").addCallback((profile: TwitterUserInternal) => { // @dangeredwolf
+			console.log("Got details for dangeredwolf", profile);
+			console.log("bout to drop this", this.developerInfoHTML(profile));
+			$("#dangeredwolf-profile")[0].innerHTML = this.developerInfoHTML(profile);
+			console.log("dropped this", $("#dangeredwolf-profile").html());
+		});
+	}
 
 	constructor() {
 		super();
@@ -34,7 +67,7 @@ export class UIWelcome extends UIModal {
 
 		window.mtdPrepareWindows();
 
-		if ($(".mtd-language-picker").length > 0) { //language > welcome
+		if ($(".mtd-language-picker").length > 0) { // language > welcome
 			return;
 		}
 
@@ -45,17 +78,61 @@ export class UIWelcome extends UIModal {
 
 		this.container.append(
 			make("h1").text(I18n(`Welcome to {productName}`).replace("{productName}", window.ModernDeck.productName)),
-			make("p").text(I18n(`Welcome to {productName}, a free and open-source Twitter client, built on the power of TweetDeck but enhanced with a modern UI and more customization.`).replace("{productName}", window.ModernDeck.productName))
+			make("p").text(I18n(`Welcome to {productName}, a free and open-source Twitter client, built on the power of TweetDeck, enhanced with a modern UI and more customization features.`).replace("{productName}", window.ModernDeck.productName))
 		)
 
-		this.leftPane = make("div").addClass("mtd-welcome-pane mtd-welcome-pane-left");
-		this.rightPane = make("div").addClass("mtd-welcome-pane mtd-welcome-pane-right");
+		this.leftPane = make("div").addClass("mtd-welcome-pane mtd-welcome-pane-left").appendTo(this.container);
+		this.rightPane = make("div").addClass("mtd-welcome-pane mtd-welcome-pane-right").appendTo(this.container);
 
 		$("#settings-modal").click(() => {
 			setPref("mtd_welcomed", true);
 		})
 
+		if (true || getPref("mtd_welcomed") === false) {
+
+			this.leftPane.append(
+				make("h2").text(I18n("New to ModernDeck?")),
+				make("p").html(I18n("Pick a theme you'd like to use. You can customize colors and more about your ModernDeck experience in <i class='icon icon-settings'></i> <b>Settings</b>.")),
+			)
+
+			this.themeBox = make("select").attr("type", "select")
+
+			const dropdown = new SettingsDropdown(themeSettings.options.theme, this.leftPane);
+			dropdown.label.remove();
+
+			if (isApp) {
+				this.leftPane.append(
+					make("h2").text(I18n("Been here before?")),
+					make("p").html(I18n("If you exported your settings from ModernDeck before, you can restore those in <i class='icon icon-settings'></i> <b>Settings › System › Load Backup</b>"))
+				);
+			}
+			
+		} else if (getPref("mtd_welcomed") === true) {
+			this.leftPane.append(
+				make("h2").text(I18n("You've been upgraded to ModernDeck 10")),
+				make("p").text(I18n("Things should look familiar around here, but with new features and a lot of under-the-hood changes. I hope you like it!"))
+			);
+		}
+
+		this.rightPane.append(
+			make("h2").text(I18n("Follow ModernDeck on Twitter")),
+			make("p").addClass("mtd-welcome-follow-header").text(I18n("Follow ModernDeck or its developer to learn about the latest updates or to contact for help.")),
+			make("div").addClass("mtd-welcome-developer-profile").attr("id","moderndeck-profile"),
+			make("div").addClass("mtd-welcome-developer-profile").attr("id","dangeredwolf-profile"),
+			make("h2").text(I18n("Help support ModernDeck")),
+			make("p").addClass("mtd-welcome-donation-header").text(I18n("ModernDeck has been developed since 2014 over countless hours of work and released for free. If you'd like to help continue its development, consider leaving a donation or becoming a sponsor.")),
+			make("a").addClass("mtd-kofi-button").append(
+				make("img").attr("src", window.mtdBaseURL + "assets/img/kofi.webp"),
+			).attr("href", "https://ko-fi.com/dangeredwolf").attr("target","_blank"),
+			make("button").addClass("mtd-sponsor-button").append(
+				make("i").addClass("Icon icon-favorite"),
+				I18n("Sponsor")
+			).click(() => open("https://github.com/sponsors/dangeredwolf/", "_blank"))
+		)
+
 		this.display();
+
+		this.renderDeveloperInfo();
 
 		return this;
 	}
