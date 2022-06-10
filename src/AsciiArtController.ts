f8518f69a8082048c675ac50031640a79f64a38f
dangered wolf <d@ngeredwolf.me>
2022-06-10 17:50:25 -0400

    Updated ASCII logo and console log

--- src/AsciiArtController.ts
+++ src/AsciiArtController.ts
@@ -11,36 +11,40 @@ export class AsciiArtController {
 
 	static draw(): void {
 
-		let text = `
-    █████████████████████████████████████████
-   ███████████████████████████████████████████
+    let consoleStyle = `
+        line-height: 1.35;
+    `
+
+    let text = `
+    ▄███████████████████████████████████████▄
+  ▄███████████████████████████████████████████▄
   █████████████████████████████████████████████
-  █████████████     ████████      ▐████████████
-  ███████████▌     ███████         ▐███████████
-  ██████████      ███████     ██    ▐██████████
-  ████████▌     ████████     ████    ▐█████████   ModernDeck ${ModernDeck.versionFriendlyString}
-  ███████▌     ███████     ███████     ████████   Build ${ModernDeck.buildNumber}
-  ████████     ██████     ███████     ▐████████   ${ModernDeck.platformName}
-  █████████▌     ███     ███████     ▐█████████
-  ███████████           ███████     ███████████   Made with love
-  ████████████        ███████     ▐████████████   by dangered wolf
+  ████████████▀  ▀███████████▀   ▀█████████████
+  ███████████      ████████▀       ▀███████████
+  ██████████     ▄████████     ▄     ██████████   ModernDeck ${ModernDeck.versionFriendlyString}
+  █████████     ████████▀     ███     ▀████████   Build ${ModernDeck.buildNumber}
+  ███████▀     ████████     ▄█████     ▀███████   ${ModernDeck.platformName}
+  ███████     ███████▀     ████████     ███████
+  ███████▌     █████▀     ████████     ████████
+  █████████     ▀██     ▄███████▀     █████████   Made with love
+  ██████████           ████████▀     ██████████   by dangered wolf
+  ███████████▄       ▄████████▌     ███████████
+  ██████████████▄▄▄█████████████▄▄█████████████
   █████████████████████████████████████████████
-  ████████████████████████████████████████████▌
-   ██████████████████████████████████████████▌
-    ████████████████████████████████████████▌
-                 ██████████████▌
-                   ██████████▌
-                     ██████▌
-`
+  ▀███████████████████████████████████████████▀
+    ▀███████████████████████████████████████▀ 
+                 ▀███████████▀
+                   ▀███████▀
+                     ▀███▀
+  `
 
 		// https://twitter.com/dangeredwolf/status/1263968859637395466
 		if (navigator.userAgent.indexOf("Chrome/") > 0) {
 			document.getElementsByTagName("html")[0].prepend(document.createComment(text));
-      console.log(text);
-		}
-
-    if (navigator.userAgent.indexOf("Firefox/") > 0) {
-      console.log(text);
+		} else if (navigator.userAgent.indexOf("Safari/") > 0) {
+      consoleStyle += `font-family: "SF Mono Regular", monospace !important;`;
     }
+    
+    console.log(`%c${text}`, consoleStyle);
 	}
 }
